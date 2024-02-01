440aae24b29b62ea05510f1edc4abfbb96d43b12
Avin Zhang <avin.zhang@example.com>
2024-02-01 15:28:56 +1000

    Updated 1s-2r setup

--- 1S-2R/clean.sh
+++ 1S-2R/clean.sh
@@ -1,2 +1,3 @@
 rm -rf ./data/*/* -y
 rm -rf ./log/*/* -y
+rm -rf ./config/clickhouse0* ./config/keeper0*
--- 1S-2R/start.sh
+++ 1S-2R/start.sh
@@ -1,80 +1,52 @@
 #!/bin/bash
 
-echo "Start the cluster"
-docker-compose up -d
+echo "* Generate configs for clickhouse nodes"
+#for keepers
+node=1
+while [ $node -le 3 ]
+do
+  mkdir -p ./config/keeper0${node}
+  node=$node envsubst < ./config/keeper.xml > ./config/keeper0${node}/keeper.xml
+  node=$((node+1))
+done
+shard_id=1
+replica_id=1
+node=1
+while [ "$node" -le 2 ]
+do
+  while [ "$shard_id" -le 1 ]
+  do
+    while [ "$replica_id" -le 2 ]
+    do
+       mkdir -p ./config/clickhouse0${node}/
+       node=$node replica_id=$replica_id shard_id=$shard_id envsubst < ./config/config.xml > ./config/clickhouse0${node}/config.xml
+       node=$((node+1))
+       replica_id=$((replica_id+1))
+    done
+    shard_id=$((shard_id+1))
+    replica_id=1
+  done
+done
 
-sleep 5
 echo
-echo "* Check clickhouse system.zookeeper table"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-select * from system.zookeeper 
-where path IN ('/', '/clickhouse')
-"
 echo
+echo "* Start keepers"
+docker-compose up -d --build --no-deps keeper01 keeper02 keeper03
+
+
+KEEPERS_STARTED=false
+while [ "$KEEPERS_STARTED" = "false" ]
+do
+    echo " Waiting for Keepers to start..."
+    keeper01_status=`docker-compose exec keeper01 bash -c "echo ruok |nc localhost 9181"`
+    keeper02_status=`docker-compose exec keeper02 bash -c "echo ruok |nc localhost 9181"`
+    keeper03_status=`docker-compose exec keeper03 bash -c "echo ruok |nc localhost 9181"`
+    if [ "$keeper01_status" = "imok" ] && [ "$keeper02_status" = "imok" ] && [ "$keeper03_status" = "imok" ]; then
+       echo " Keepers are started and ready"
+       KEEPERS_STARTED=true
+    fi
+    sleep 5
+done
 echo
-echo "* Create database"
-docker exec -it clickhouse01 bash -c 'clickhouse-client -h localhost -q "CREATE DATABASE db1 ON CLUSTER 'cluster_1S_2R';"'
-echo
-echo
-echo "* List created shards and replicas"
-docker exec -it clickhouse01 bash -c 'clickhouse-client -h localhost -q "
-SELECT
-    cluster,
-    shard_num,
-    replica_num
-FROM system.clusters
-WHERE cluster = '"'cluster_1S_2R'"'
-ORDER BY
-    shard_num ASC,
-    replica_num ASC;
-"'
-
-echo "Create table on db1"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-CREATE TABLE db1.events on cluster 'cluster_1S_2R'
-(
-    time DateTime,
-    uid Int64,
-    type LowCardinality(String)
-)
-ENGINE = ReplicatedMergeTree('/clickhouse/tables/cluster_1S_2R/{shard}/events', '{replica}')
-PARTITION BY toDate(time)
-ORDER BY uid;
-"
-
-
-echo "Create distributed table to represent the data on the shards"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-CREATE TABLE db1.dist_table ON CLUSTER 'cluster_1S_2R'
-  AS db1.events
-  ENGINE = Distributed('cluster_1S_2R', db1, events, rand());
-"
-
-
-echo "Insert rows into table on node clickhouse01"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-INSERT INTO db1.dist_table VALUES
-    ('2020-01-01 10:00:00', 100, 'view'),
-    ('2020-01-01 10:05:00', 101, 'view'),
-    ('2020-01-01 11:00:00', 100, 'contact'),
-    ('2020-01-01 12:10:00', 101, 'view'),
-    ('2020-01-02 08:10:00', 100, 'view'),
-    ('2020-01-03 13:00:00', 103, 'view');
-"
-
-echo "Select data from clickhouse01"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-select * from db1.events;
-"
-
-echo "Select rows from clickhouse02"
-docker exec -it clickhouse02 clickhouse-client -h localhost -q "
-select * from db1.events;
-"
-
-echo "Read from distributed table"
-docker exec -it clickhouse01 clickhouse-client -h localhost -q "
-select * from db1.dist_table;
-"
-echo 
-exit
+echo "* Start clickhouse nodes"
+docker-compose up -d --build --no-deps clickhouse01 clickhouse02
