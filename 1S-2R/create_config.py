6bfd40aa94645b5648bd834252bfb3913c17fb18
Avin Zhang <avin.zhang@example.com>
2024-02-03 14:06:41 +1000

    Add part to create_configs.py to create config.xml

--- 1S-2R/create_config.py
+++ 1S-2R/create_config.py
@@ -3,24 +3,16 @@ import os
 
 import click
 
-@click.command()
-@click.option("-s", "--num_of_shards", default=1, help="Number of shards to create. Default is 1")
-@click.option("-r", "--num_of_replicas", default=2, help="Number of replicas to create. Default is 2")
-
-
-def create_configs(num_of_shards, num_of_replicas):
-  cluster_name = 'cluster_'+ str(num_of_shards) + 'S_' + str(num_of_replicas) + 'R'
-  num_of_nodes = num_of_shards * num_of_replicas
+def create_cluster(shards, replicas):
+  cluster_name = 'cluster_'+ str(shards) + 'S_' + str(replicas) + 'R'
+  print(cluster_name)
+  num_of_nodes = replicas * shards 
   node_id = 0
   while node_id < num_of_nodes:
-    path = './config/clickhouse0' + str(node_id + 1)
-    if not os.path.exists(path):
-      os.mkdir(path)
-    
     root = ET.Element('clickhouse')
     #Marcos
-    mshard_id = node_id // num_of_replicas
-    mreplica_id = node_id % num_of_replicas
+    mshard_id = node_id // replicas
+    mreplica_id = node_id % replicas
     macro = ET.SubElement(root, 'macros')
     m_shard = ET.SubElement(macro, 'shard')
     m_shard.text = str(mshard_id)
@@ -32,12 +24,12 @@ def create_configs(num_of_shards, num_of_replicas):
     node = 1
     rs = ET.SubElement(root, 'remote_server')
     cluster = ET.SubElement(rs, cluster_name)
-    while shard_id <= num_of_shards:
+    while shard_id <= shards:
       shard = ET.SubElement(cluster, 'shard')
       internal_replication = ET.SubElement(shard, 'internal_replication')
       internal_replication.text = "true"
       replica_id = 0
-      while replica_id < num_of_replicas:
+      while replica_id < replicas:
         replica = ET.SubElement(shard, 'replica')
         host = ET.SubElement(replica, 'host')
         host.text = 'clickhouse0' + str(node)
@@ -52,7 +44,31 @@ def create_configs(num_of_shards, num_of_replicas):
     tree.write('./config/clickhouse0' + str(node_id + 1) + '/cluster.xml')
     node_id+=1  
 
+@click.command()
+@click.option("-s", "--num_of_shards", default=1, help="Number of shards to create. Default is 1")
+@click.option("-r", "--num_of_replicas", default=2, help="Number of replicas to create. Default is 2")
+def create_config(num_of_shards, num_of_replicas):
+  num_of_nodes = num_of_shards * num_of_replicas
+  node_id = 0
+  while node_id < num_of_nodes:  
+    path = './config/clickhouse0' + str(node_id + 1)
+    if not os.path.exists(path):
+      os.mkdir(path)
 
+    tree = ET.parse('./config/config.xml')
+    root = tree.getroot()
+    dname = root.find("display_name")
+    dname.text = 'clickhouse0' + str(node_id + 1)
+    httphost = root.find("interserver_http_host")
+    httphost.text = 'clickhouse0' + str(node_id + 1)
+    tree = ET.ElementTree(root)
+    tree.write('./config/clickhouse0' + str(node_id + 1) + '/config.xml')
+    node_id+=1
+  create_cluster(num_of_shards, num_of_replicas)
 
-if __name__ == '__main__':
-  create_configs()
+def main():
+  create_config()
+  
+
+if __name__ == "__main__":
+    main()
