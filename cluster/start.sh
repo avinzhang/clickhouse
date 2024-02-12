2ee701cee4657d0e7e3de9d6f4f05421206cb922
Avin Zhang <avin.zhang@example.com>
2024-02-12 16:38:08 +1000

    Updated to remove comment

--- cluster/start.sh
+++ cluster/start.sh
@@ -8,34 +8,34 @@ num_of_shards=1
 num_of_replicas=2
 python3 create_config.py -s $num_of_shards -r $num_of_replicas
 num_of_nodes=$((num_of_shards * num_of_replicas))
-#ch_nodes=""
-#i=1
-#while [ $i -le $num_of_nodes ]
-#  do
-#    ch_nodes=`echo $ch_nodes | sed -e "s/$/ clickhouse0$i/"`
-#    i=$(( $i + 1 ))
-#done
-#echo
-#echo "* Start keepers"
-#docker-compose up -d --build --no-deps keeper01 keeper02 keeper03
-#
-#
-#KEEPERS_STARTED=false
-#while [ "$KEEPERS_STARTED" = "false" ]
-#do
-#    echo " Waiting for Keepers to start..."
-#    keeper01_status=`docker-compose exec keeper01 bash -c "echo ruok |nc localhost 9181"`
-#    keeper02_status=`docker-compose exec keeper02 bash -c "echo ruok |nc localhost 9181"`
-#    keeper03_status=`docker-compose exec keeper03 bash -c "echo ruok |nc localhost 9181"`
-#    if [ "$keeper01_status" = "imok" ] && [ "$keeper02_status" = "imok" ] && [ "$keeper03_status" = "imok" ]; then
-#       echo " Keepers are started and ready"
-#       KEEPERS_STARTED=true
-#    fi
-#    sleep 5
-#done
-#echo
-#echo "* Start clickhouse nodes"
-#docker-compose up -d --build --no-deps $ch_nodes
+ch_nodes=""
+i=1
+while [ $i -le $num_of_nodes ]
+  do
+    ch_nodes=`echo $ch_nodes | sed -e "s/$/ clickhouse0$i/"`
+    i=$(( $i + 1 ))
+done
+echo
+echo "* Start keepers"
+docker-compose up -d --build --no-deps keeper01 keeper02 keeper03
+
+
+KEEPERS_STARTED=false
+while [ "$KEEPERS_STARTED" = "false" ]
+do
+    echo " Waiting for Keepers to start..."
+    keeper01_status=`docker-compose exec keeper01 bash -c "echo ruok |nc localhost 9181"`
+    keeper02_status=`docker-compose exec keeper02 bash -c "echo ruok |nc localhost 9181"`
+    keeper03_status=`docker-compose exec keeper03 bash -c "echo ruok |nc localhost 9181"`
+    if [ "$keeper01_status" = "imok" ] && [ "$keeper02_status" = "imok" ] && [ "$keeper03_status" = "imok" ]; then
+       echo " Keepers are started and ready"
+       KEEPERS_STARTED=true
+    fi
+    sleep 5
+done
+echo
+echo "* Start clickhouse nodes"
+docker-compose up -d --build --no-deps $ch_nodes
 echo
 CLICKHOUSE_STARTED=false
 while [ "$CLICKHOUSE_STARTED" = "false" ]
