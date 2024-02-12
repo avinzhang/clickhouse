85d77766707d5ad2edf6da7b24c6282a20250e74
Avin Zhang <avin.zhang@example.com>
2024-02-12 15:22:23 +1000

    Updated grafana setup

--- grafana/start.sh
+++ grafana/start.sh
@@ -1,51 +1,48 @@
 #!/bin/bash
 
-echo "* Generate configs for clickhouse nodes"
-#for keepers
-node=1
-while [ $node -le 3 ]
-do
-  mkdir -p ./config/keeper0${node}
-  node=$node envsubst < ./config/keeper.xml > ./config/keeper0${node}/keeper.xml
-  node=$((node+1))
-done
-shard_id=1
-replica_id=1
-node=1
-while [ "$node" -le 2 ]
-do
-  while [ "$shard_id" -le 1 ]
+num_of_shards=1
+num_of_replicas=2
+python3 create_config.py -s $num_of_shards -r $num_of_replicas
+num_of_nodes=$((num_of_shards * num_of_replicas))
+ch_nodes=""
+i=1
+while [ $i -le $num_of_nodes ]
   do
-    while [ "$replica_id" -le 2 ]
-    do
-       mkdir -p ./config/clickhouse0${node}/
-       node=$node replica_id=$replica_id shard_id=$shard_id envsubst < ./config/config.xml > ./config/clickhouse0${node}/config.xml
-       node=$((node+1))
-       replica_id=$((replica_id+1))
-    done
-    shard_id=$((shard_id+1))
-    replica_id=1
-  done
+    ch_nodes=`echo $ch_nodes | sed -e "s/$/ clickhouse0$i/"`
+    i=$(( $i + 1 ))
 done
-
-
-
+echo "* Starting up clickhouse keepers"
 docker-compose up -d --build --no-deps keeper01 keeper02 keeper03
 
 
 KEEPERS_STARTED=false
 while [ "$KEEPERS_STARTED" = "false" ]
 do
-    echo "Waiting for Keepers to start..."
+    echo "   Waiting for Keepers to start..."
     keeper01_status=`docker-compose exec keeper01 bash -c "echo ruok |nc localhost 9181"`
     keeper02_status=`docker-compose exec keeper02 bash -c "echo ruok |nc localhost 9181"`
     keeper03_status=`docker-compose exec keeper03 bash -c "echo ruok |nc localhost 9181"`
     if [ "$keeper01_status" = "imok" ] && [ "$keeper02_status" = "imok" ] && [ "$keeper03_status" = "imok" ]; then
-       echo "Keepers are started and ready"
+       echo "  Keepers are started and ready"
        KEEPERS_STARTED=true
     fi
     sleep 5
 done
 
-docker-compose up -d --build --no-deps clickhouse01 clickhouse02
+echo "* Starting up clickhouse nodes"
+docker-compose up -d --build --no-deps $ch_nodes
 
+CLICKHOUSE_STARTED=false
+while [ "$CLICKHOUSE_STARTED" = "false" ]
+do
+    echo "   Waiting for Clickhouse nodes to start..."
+    clickhouse01_status=`curl -s http://localhost:8123`
+    clickhouse02_status=`curl -s http://localhost:8124`
+    if [ "$clickhouse01_status" == "Ok." ] && [ "$clickhouse02_status" == "Ok." ]; then
+       echo "  Clickhouse nodes are started and ready"
+       CLICKHOUSE_STARTED=true
+    fi
+    sleep 5
+done
+
+docker-compose up -d --build --no-deps grafana
