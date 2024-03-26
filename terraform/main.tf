72340db65d945473a6c1f879cb6c65d1a6e41b9c
Avin Zhang <avin.zhang@example.com>
2024-03-27 09:55:44 +1000

    Added terraform setup of clickhouse

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    clickhouse = {
+      version = "0.0.4"
+      source  = "ClickHouse/clickhouse"
+    }
+  }
+}
+
+variable "organization_id" {
+  type = string
+}
+
+variable "token_key" {
+  type = string
+}
+
+variable "token_secret" {
+  type = string
+}
+
+provider clickhouse {
+  organization_id = var.organization_id
+  token_key       = var.token_key
+  token_secret    = var.token_secret
+}
+
+resource "clickhouse_service" "service" {
+  name                      = "avin-tf"
+  cloud_provider            = "aws"
+  region                    = "ap-southeast-2"
+  tier                      = "production"
+  idle_scaling              = true
+  password_hash             = "n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=" # base64 encoded sha256 hash of "test"
+
+  ip_access = [
+    {
+      source      = "0.0.0.0"
+      description = "Test IP"
+    }
+  ]
+
+  min_total_memory_gb  = 48
+  max_total_memory_gb  = 120
+  idle_timeout_minutes = 5
+}
+
+output "service_endpoints" {
+  value = clickhouse_service.service.endpoints
+}
+
+output "service_iam" {
+  value = clickhouse_service.service.iam_role
+}
