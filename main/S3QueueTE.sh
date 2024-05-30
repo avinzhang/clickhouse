4141842aadcc7221ec8baebf84aaa67fb6a84ddc
Avin Zhang <avin.zhang@example.com>
2024-05-30 20:49:16 +1000

    Updated keeper check code

--- main/S3QueueTE.sh
+++ main/S3QueueTE.sh
@@ -33,9 +33,9 @@ KEEPERS_STARTED=false
 while [ "$KEEPERS_STARTED" = "false" ]
 do
     echo "   Waiting for Keepers to start..."
-    keeper01_status=`docker-compose exec keeper01 bash -c "echo ruok |nc localhost 9181"`
-    keeper02_status=`docker-compose exec keeper02 bash -c "echo ruok |nc localhost 9181"`
-    keeper03_status=`docker-compose exec keeper03 bash -c "echo ruok |nc localhost 9181"`
+    keeper01_status=`echo ruok |nc localhost 9181`
+    keeper02_status=`echo ruok |nc localhost 9181`
+    keeper03_status=`echo ruok |nc localhost 9181`
     if [ "$keeper01_status" = "imok" ] && [ "$keeper02_status" = "imok" ] && [ "$keeper03_status" = "imok" ]; then
        echo "  Keepers are started and ready"
        KEEPERS_STARTED=true
